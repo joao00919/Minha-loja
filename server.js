456343a2e3c146b90dbd101c2ae28e60714aa1c5
joao00919 <jvgwmer3@gmail.com>
2026-03-28 21:53:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -33,4 +33,5 @@ app.post("/checkout", async (req, res) => {
   res.json(response.data);
 });
 
-app.listen(3000, () => console.log("🔥 Rodando"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("🔥 Rodando"));
